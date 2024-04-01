12d5d6535be0cf940154609974050a721a045f08
Juan <juandiegovelsol@gmail.com>
2024-04-01 16:50:01 -0500

    feat: se genera texto en el pdf

--- src/controllers/pdf.js
+++ src/controllers/pdf.js
@@ -1,11 +1,11 @@
-import { PDFDocument } from "pdf-lib";
+import { PDFDocument, rgb, StandardFonts } from "pdf-lib";
 import { readFile, writeFile } from "fs/promises";
 
 export const pdfStream = async (req, res) => {
   try {
     const pdfDoc = "This is a pdf file";
     await createPdf("pdf/prueba.pdf", "informe.pdf");
-    res.status(200).json({ pdf });
+    res.status(200).json({ pdfDoc });
   } catch (error) {
     console.log(error);
   }
@@ -14,6 +14,19 @@ export const pdfStream = async (req, res) => {
 const createPdf = async (input, output) => {
   try {
     const pdfDoc = await PDFDocument.load(await readFile(input));
+    const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
     console.log(pdfDoc);
-  } catch (error) {}
+    const page = pdfDoc.getPages()[0];
+    page.drawText("Nuevo texto", {
+      x: 5,
+      y: 5,
+      size: 50,
+      font: helveticaFont,
+      color: rgb(0.95, 0.1, 0.1),
+    });
+    const pdfBytes = await pdfDoc.save();
+    await writeFile(output, pdfBytes);
+  } catch (error) {
+    console.log(error);
+  }
 };
